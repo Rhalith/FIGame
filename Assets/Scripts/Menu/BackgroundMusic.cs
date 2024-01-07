e0ccba83c4632f218ab9ffeac941e04df4cf23f3
Rhalith <akmannuhyigit@gmail.com>
2024-01-07 17:45:17 +0300

    Fix: volume settings

--- Assets/Scripts/Menu/BackgroundMusic.cs
+++ Assets/Scripts/Menu/BackgroundMusic.cs
@@ -6,9 +6,11 @@ namespace Scripts.Menu
     public class BackgroundMusic : MonoBehaviour
     {
         [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioMixer _audioMixer;
         public void PlayMusic()
         {
             _audioSource.Play();
+            _audioMixer.SetFloat("mainvolume", -50f);
         }
     }
 }
--- Assets/Scripts/Menu/SetVolume.cs
+++ Assets/Scripts/Menu/SetVolume.cs
@@ -6,6 +6,11 @@ namespace Scripts.Menu
     public class SetVolume : MonoBehaviour
     {
         [SerializeField] private AudioMixer _audioMixer;
+        private void Awake()
+        {
+            _audioMixer.SetFloat("mainvolume", -50f);
+        }
+
         public void AdjustVolume(float volume)
         {
             _audioMixer.SetFloat("mainvolume", volume);

f61c46f3213b6a97282d6c79471b40cb32f0aee1
Nuh Yiğit Akman <akmannuhyigit@gmail.com>
2024-12-17 21:10:35 +0300

    Add: tyre animation for movement

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -9,19 +9,25 @@ namespace Scripts.Managers
     public class PlayerManager : MonoBehaviour
     {
         #region Player Scripts
-        [Header("Player Scripts")]
-        [SerializeField] private PlayerAnimation _playerAnimation;
+
+        [Header("Player Scripts")] [SerializeField]
+        private PlayerAnimation _playerAnimation;
+
         [SerializeField] private PlayerMovement _playerMovement;
+
         #endregion
-        
+
         #region Other Components
-        [Header("Other Components")]
-        [SerializeField] private GameManager _gameManager;
+
+        [Header("Other Components")] [SerializeField]
+        private GameManager _gameManager;
+
         [SerializeField] private ScrollBackground _scrollBackground;
         [SerializeField] private GameObject _tireMenu;
         [SerializeField] private Animator _animator;
         [SerializeField] private Rigidbody2D _rigidbody;
         [SerializeField] private GameObject _deathScreen;
+
         #endregion
 
         private void OnEnable()
@@ -30,7 +36,7 @@ namespace Scripts.Managers
             EventBus<PitStopEvent>.AddListener(PitStop);
             EventBus<PlayerDeathEvent>.AddListener(KillPlayer);
         }
-        
+
         private void OnDisable()
         {
             EventBus<PitLaneEntranceEvent>.RemoveListener(PitLaneEntrance);
@@ -40,24 +46,27 @@ namespace Scripts.Managers
 
         private void KillPlayer(object sender, PlayerDeathEvent @event)
         {
-            DOTween.To(() => _scrollBackground.Speed, x => _scrollBackground.Speed = x, 0, 1f).OnComplete(() => _deathScreen.SetActive(true));
+            DOTween.To(() => _scrollBackground.Speed, x => _scrollBackground.Speed = x, 0, 1f)
+                .OnComplete(() => _deathScreen.SetActive(true));
             EventBus<CheckSelectableElementEvent>.Emit(this, new CheckSelectableElementEvent { CanSelect = true });
+            EventBus<TyreAnimationEvent>.Emit(this, new TyreAnimationEvent { ShouldStop = true });
         }
 
         private void PitStop(object sender, PitStopEvent @event)
         {
             if (@event.IsStart)
             {
-                ChangeBackGroundSpeed(0,1f);
+                ChangeBackGroundSpeed(0, 1f);
                 _tireMenu.SetActive(true);
+                EventBus<TyreAnimationEvent>.Emit(this, new TyreAnimationEvent { ShouldStop = true });
                 EventBus<CheckSelectableElementEvent>.Emit(this, new CheckSelectableElementEvent { CanSelect = true });
-                
             }
             else
             {
-                ChangeBackGroundSpeed(0.2f,1f);
+                ChangeBackGroundSpeed(0.2f, 1f);
                 _animator.SetBool("isTireSelected", true);
                 _tireMenu.SetActive(false);
+                EventBus<TyreAnimationEvent>.Emit(this, new TyreAnimationEvent { ShouldStop = false });
                 EventBus<CheckSelectableElementEvent>.Emit(this, new CheckSelectableElementEvent { CanSelect = false });
             }
         }
@@ -75,8 +84,8 @@ namespace Scripts.Managers
                 _animator.enabled = false;
             }
         }
-        
-        
+
+
         private void ChangeBackGroundSpeed(float speed, float duration)
         {
             DOTween.To(() => _scrollBackground.Speed, x => _scrollBackground.Speed = x, speed, duration);
---
+++ Assets/Scripts/Player/TyreAnimationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Scripts.Events;
+using Scripts.EventBus;
+using UnityEngine;
+
+namespace Scripts.Player
+{
+    public class TyreAnimationController : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+
+        private void OnEnable()
+        {
+            EventBus<TyreAnimationEvent>.AddListener(StartTyreAnimation);
+        }
+        
+        private void OnDisable()
+        {
+            EventBus<TyreAnimationEvent>.RemoveListener(StartTyreAnimation);
+        }
+
+        private void StartTyreAnimation(object sender, TyreAnimationEvent @event)
+        {
+            _animator.SetBool("inPit", @event.ShouldStop);
+        }
+    }
+}

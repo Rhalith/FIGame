8f07fe06fb0bc8cbf1d64247093c8c193bfec963
Nuh Yiğit Akman <akmannuhyigit@gmail.com>
2024-12-30 19:31:21 +0300

    Fix: car rotation bug

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,13 +10,18 @@ namespace Scripts.Player
     {
         [SerializeField] private PlayerSpecs _playerSpecs;
         [SerializeField] private Rigidbody2D _rigidbody;
+
+        [SerializeField] private float _xBound;
+        [SerializeField] private float _yBound;
+        [SerializeField] private float _safetyCarXBound;
         private Vector2 _movement;
-        
+
         private Vector2 _startPosition;
-        
+
         private bool _shouldReset;
         private bool _canMove;
-        
+        private bool _safetyCarBoundsActive;
+
         private void Start()
         {
             _startPosition = transform.position;
@@ -26,12 +31,16 @@ namespace Scripts.Player
         {
             EventBus<ResetCarPositionEvent>.AddListener(ResetCarPosition);
             EventBus<SetPlayerMovementEvent>.AddListener(ChangeMovement);
+            EventBus<CallSafetyCarEvent>.AddListener(ActivateSafetyCarBounds);
+            EventBus<SendSafetyCarEvent>.AddListener(DeactivateSafetyCarBounds);
         }
-        
+
         private void OnDisable()
         {
             EventBus<ResetCarPositionEvent>.RemoveListener(ResetCarPosition);
             EventBus<SetPlayerMovementEvent>.RemoveListener(ChangeMovement);
+            EventBus<CallSafetyCarEvent>.RemoveListener(ActivateSafetyCarBounds);
+            EventBus<SendSafetyCarEvent>.RemoveListener(DeactivateSafetyCarBounds);
         }
 
         private void ChangeMovement(object sender, SetPlayerMovementEvent @event)
@@ -44,10 +53,21 @@ namespace Scripts.Player
             _shouldReset = true;
         }
 
+        private void ActivateSafetyCarBounds(object sender, CallSafetyCarEvent @event)
+        {
+            _safetyCarBoundsActive = true;
+        }
+
+        private void DeactivateSafetyCarBounds(object sender, SendSafetyCarEvent @event)
+        {
+            _safetyCarBoundsActive = false;
+        }
+
         public void OnMove(InputAction.CallbackContext obj)
         {
             _movement = obj.ReadValue<Vector2>();
         }
+
         private void FixedUpdate()
         {
             MovePlayer();
@@ -63,8 +83,24 @@ namespace Scripts.Player
             }
             else
             {
-                if(!_canMove) _rigidbody.velocity = Vector2.zero;
-                else _rigidbody.velocity = new Vector2(_movement.x, _movement.y) * (_playerSpecs.PlayerSpeed * 200);
+                if (!_canMove)
+                {
+                    _rigidbody.velocity = Vector2.zero;
+                }
+                else
+                {
+                    Vector2 newVelocity = new Vector2(_movement.x, _movement.y) * (_playerSpecs.PlayerSpeed * 200);
+                    _rigidbody.velocity = newVelocity;
+                    float maxX = _safetyCarBoundsActive ? _safetyCarXBound : _xBound;
+
+                    Vector3 clampedPosition = new Vector3(
+                        Mathf.Clamp(transform.localPosition.x, -_xBound, maxX),
+                        Mathf.Clamp(transform.localPosition.y, -_yBound, _yBound),
+                        transform.localPosition.z
+                    );
+
+                    transform.localPosition = clampedPosition;
+                }
             }
         }
     }
